20a2ee00e471aca9e1fe7e17220fa28559039120
anamaydev <anamaydev@gmail.com>
2025-05-25 23:45:43 +0530

    style: add responsive styles for tablet and desktop views

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,15 @@ import checkboxUnchecked from './assets/images/icon-checkbox-unchecked.svg'
 function App() {
   return (
     /* background */
-    <div className="min-h-dvh w-screen bg-green-200">
+    <div className="min-h-dvh w-screen px-2 py-4 flex flex-col justify-center items-center bg-green-200 sm:px-5">
       {/* form */}
-      <form action="/" className="p-5 flex flex-col justify-center gap-5 bg-white rounded-2xl">
+      <form action="/" className="p-3 flex flex-col justify-center gap-5 w-full max-w-(--form-max) bg-white rounded-2xl">
         {/* first div */}
         <div className="flex flex-col justify-center gap-4">
           <h1 className="font-bold text-lg leading-none text-grey-900">Contact us</h1>
           <div className="flex flex-col gap-3">
             {/* name inputs */}
-            <div className="flex flex-col justify-center gap-3">
+            <div className="flex flex-col justify-center gap-3 sm:flex-row sm:gap-2 sm:justify-stretch">
               {/* first name */}
               <div className="text-input-container">
                 <label htmlFor="first-name" className="label">First Name</label>
@@ -62,14 +62,14 @@ function App() {
               {/* fake legend */}
               <p className="label" id="query-legend">Query Type</p>
 
-              <ul className="flex flex-col justify-center gap-2"> {/*remove gap-2*/}
-                <li className="input-field flex gap-1.5">
+              <ul className="flex flex-col justify-center gap-2 sm:flex-row sm:justify-stretch"> {/*remove gap-2*/}
+                <li className="input-field flex gap-1.5 w-full">
                   <img src={radioUnselected} alt="" className=""/>
                   <img src={radioSelected} alt="" className="hidden"/>
                   <input type="radio" id="general-enquiry" name="query-type"/>
                   <label htmlFor="general-enquiry" className="cursor-pointer">General Enquiry</label>
                 </li>
-                <li className="input-field flex gap-1.5">
+                <li className="input-field flex gap-1.5 w-full">
                   <img src={radioUnselected} alt="" className=""/>
                   <img src={radioSelected} alt="" className="hidden"/>
                   <input type="radio" id="support-enquiry" name="query-type"/>
@@ -80,7 +80,7 @@ function App() {
 
             <div className="text-input-container">
               <label htmlFor="message" className="label">Message</label>
-              <textarea name="message" id="message" cols="30" rows="8" className="input-field"></textarea>
+              <textarea name="message" id="message" cols="30" rows="8" className="input-field sm:h-[8.375rem]"></textarea>
             </div>
           </div>
         </div>
@@ -90,7 +90,7 @@ function App() {
           <img src={checkboxUnchecked} alt=""/>
           <img src={checkboxChecked} alt="" className="hidden"/>
           <input type="checkbox" id="concent"/>
-          <label htmlFor="concent">I consent to being contacted by the team</label>
+          <label htmlFor="concent" className="label">I consent to being contacted by the team</label>
         </div>
 
         <button className="font-bold text-md leading-normal text-white px-5 py-2 border-none rounded-lg bg-green-600">Submit</button>
